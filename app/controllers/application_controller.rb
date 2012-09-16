99c7f86704a8459f8a580e5fa72f43d111249c1d
Victor Pudeyev <piousbox@gmail.com>
2012-09-15 18:09:25 -0700

    no more ish-lib here.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,19 @@ class ApplicationController < ActionController::Base
   
   before_filter :set_action_name
   
+  private
+  
+  def set_action_name
+    begin
+      @action_name = params[:controller].gsub('/', '_') + '_' + params[:action]
+    rescue
+    end
+    
+    begin
+      @action_classes = params[:controller].gsub('/', '_')
+    rescue
+    end
+    
+  end
+  
 end
